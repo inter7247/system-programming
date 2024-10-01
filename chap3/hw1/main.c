d30e2a56afba530a6e27dcbdb3187734653efd39
inter7247 <wb2477@naver.com>
2024-10-01 17:15:33 +0900

    hw1

--- chap3/hw1/main.c
+++ chap3/hw1/main.c
@@ -2,7 +2,7 @@
 #include <string.h>
 #include "copy.h"
 
-char line[5][MAXLINE];
+char line[5][MAXLINE]; //2차원 배열로 5개 입력
 char temp[MAXLINE];
 
 void copy(char from[], char to[]);
@@ -10,11 +10,11 @@ void copy(char from[], char to[]);
 int main() {
 	int len, max = 0;
 
-	for (int i = 0; i < 5; i++) {
+	for (int i = 0; i < 5; i++) { //문자열 5개 입력받기
 		fgets(line[i], MAXLINE, stdin);
 	}
 	
-	for (int i = 0; i < 4; i++) {
+	for (int i = 0; i < 4; i++) { //temp 배열을 이용해 문자열 수 비교하여 정렬
 		for (int j = 0; j < 4; j++) {
 			if (strlen(line[j]) < strlen(line[j + 1])) {
 				copy(line[j], temp);
@@ -23,9 +23,11 @@ int main() {
 			}
 		}
 	}
+
+	printf("\n");
 	printf("\n");
 
-	for (int i = 0; i < 5; i++) {
+	for (int i = 0; i < 5; i++) { //결과 값 출력
 		printf("%s", line[i]);
 	}
 
